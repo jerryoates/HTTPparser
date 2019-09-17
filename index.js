f5f7baa91bc68e6258543250bcae2b28b8de3956
Jerry Oates <jerryoates1@gmail.com>
2019-09-17 18:42:20 -0400

    fixed headers test

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 module.exports = function parseHttp(request){
-  const values = request.split(' ')
   const parsedMessage = {}
-  parsedMessage.verb = values[0]
-  parsedMessage.path = values[1]
-  parsedMessage.version = values[2]
+  const lines = request.split('\n')
+  let parts = lines[0].split(' ')
+  parsedMessage.verb = parts[0].toUpperCase()
+  parsedMessage.path = parts[1]
+  parsedMessage.version = parts[2]
+
+
+
+  if (lines[1] === undefined){
+    return parsedMessage
+  }
+
+  parsedMessage.headers = {}
+  parts = lines[1].split(': ')
+  console.log(parts)
+  const key = parts[0]
+  const value = parts[1]
+  parsedMessage.headers[key] = value
   return parsedMessage
 }
--- spec/parserSpec.js
+++ spec/parserSpec.js
@@ -14,4 +14,33 @@ describe('parses http', function(){
     }
     expect(output).toEqual(expected)
   })
+  it ('normalizes the verb', function(){
+    //arrange
+    const request = 'get / HTTP/1.1'
+    //act
+    const output = parseHttp(request)
+    //assert
+    const expected = {
+      verb: 'GET',
+      path: '/',
+      version: 'HTTP/1.1'
+    }
+    expect(output).toEqual(expected)
+  })
+  it ('sets content-type headers', function(){
+    //arrange
+    const request = "get / HTTP/1.1\ncontent-type: text/html; charset=utf-8"
+    //act
+    const output = parseHttp(request)
+    //assert
+    const expected = {
+      verb: 'GET',
+      path: '/',
+      version: 'HTTP/1.1',
+      headers: {
+        'content-type': 'text/html; charset=utf-8'
+    }
+  }
+    expect(output).toEqual(expected)
+})
 })
